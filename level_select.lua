57522ed9544d0f529de94d2410932e7f768396c4
Alex De Meo <alexander.demeo@ocsbstudent.ca>
2020-01-07 15:34:46 -0500

    did work

--- level_select.lua
+++ level_select.lua
@@ -25,8 +25,11 @@ display.setStatusBar(display.HiddenStatusBar)
 local backButton
 local levelSelectText
 local level1Button
-local level3Button
 local level1Text
+local level2Button
+local level2Text
+local level3Button
+local level3Text
 
 ------------------------------------------------------------------------
 --LOCAL FUNCTIONS
@@ -39,6 +42,11 @@ end
 -- create transition to level 1
 local function Level1Transition( )
     composer.gotoScene("level1_screen", {effect= "fade", time = 500} )
+end
+
+--create transition to level 2 
+local function Level2Transition(  )
+    composer.gotoScene("Level2_screen", {effect= "fade", time = 500})
 end
 
 -- create transition to level 3
@@ -57,12 +65,12 @@ function scene:create( event )
     backButton = widget.newButton( 
     {
         -- Setting Position
-        x = display.contentWidth*2/8,
+        x = display.contentWidth*1/8,
         y = display.contentHeight*14/16,
 
         -- Setting Dimensions
-        width = 200,
-        height = 100,
+        width = 100,
+        height = 50,
 
         -- Setting Visual Properties
         defaultFile = "Images/backButtonUnpressedAlex.png",
@@ -78,11 +86,11 @@ function scene:create( event )
     {
         -- setting the Position
         x = display.contentWidth/3,
-        y = display.contentHeight/2,
+        y = display.contentHeight*2/5,
 
         -- set Dimensions
-        width = 200,
-        height = 200,
+        width = 150,
+        height = 150,
         -- setting Visual Properties
         defaultFile = "Images/Level1ScreenAlex.png",
         overFile = "Images/level1ButtonPressed.png",
@@ -91,16 +99,35 @@ function scene:create( event )
         onRelease = Level1Transition
     } )
 
+    -- Create level 2 button 
+    level2Button = widget.newButton(
+    {
+        -- setting the Position
+        x = display.contentWidth*2/3,
+        y = display.contentHeight*2/5,
+
+        -- set Dimensions
+        width = 150,
+        height = 150,
+        -- setting Visual Properties
+        defaultFile = "Images/Ferdous 2/level2_screenFerdous.png",
+        overFile = "Images/Level2ButtonPressed.png",
+
+        -- set functional Properties
+        --onRelease = Level2Transition
+
+    } )
+
     -- create level 3 buttons
     level3Button = widget.newButton(
     {
         -- setting its position 
-        x = display.contentWidth*2/3,
-        y = display.contentHeight/2,
+        x = display.contentWidth/3,
+        y = display.contentHeight*7/10,
 
         -- set dimensions 
-        width = 200, 
-        height = 200,
+        width = 150, 
+        height = 150,
 
         -- setting the visual properties 
         defaultFile = "Images/Level3ScreenAlex.png",
@@ -114,11 +141,14 @@ function scene:create( event )
     levelSelectText = display.newText("LEVELS", display.contentWidth/2, display.contentHeight/5, nil, 100)
     levelSelectText:setTextColor(0, 0, 0)
 
-    -- create level1Text
-    level1Text = display.newText("Level 1", display.contentWidth/3, display.contentHeight*2/3, nil, 60)
+    -- create level Texts
+    level1Text = display.newText("Level 1", display.contentWidth/3 + 3, display.contentHeight*7/13, nil, 50)
     level1Text:setTextColor(0, 0, 0)
 
-    level3Text = display.newText("Level 3", display.contentWidth*2/3, display.contentHeight*2/3, nil, 60)
+    level2Text = display.newText("Level 2", display.contentWidth*2/3, display.contentHeight*7/13, nil, 48)
+    level2Text:setTextColor(0, 0, 0)
+
+    level3Text = display.newText("Level 3", display.contentWidth/3, display.contentHeight*11/13, nil, 49)
     level3Text:setTextColor(0, 0, 0)
 
     -- add buttons, objects and images to sceneGroup
@@ -126,10 +156,10 @@ function scene:create( event )
     sceneGroup:insert( levelSelectText )
     sceneGroup:insert( level1Button )
     sceneGroup:insert( level1Text )
+    sceneGroup:insert( level2Button ) 
+    --sceneGroup:insert( level2Text )
     sceneGroup:insert( level3Button )
     sceneGroup:insert( level3Text )
-
-
 end
 
 -- the function is called when the scene is still off screen (but is about to come on screen)
